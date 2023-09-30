dd42eae72b8f5c7d0ece934157d45167d50d7acf
James Teahan <sammyteahan@gmail.com>
2023-09-30 11:58:43 -0600

    Beginning to scaffold layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-[100vh] overflow-auto scrollbar-hide">
+      <header className="flex max-w-[35-rem] py-5 px-[12px] mx-auto w-full justify-between items-center">
+      </header>
     </main>
   )
 }
