2f65dbc32e7122a9a62ad5c10fbc36ce81c2dc70
sammyteahan <sammyteahan@gmail.com>
2023-10-02 20:37:28 -0600

    Small cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
         </div>
         <div className="flex w-full justify-end">
           <Button variant="outline" size="icon" onClick={toggleTheme}>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
           </Button>
         </div>
       </header>
