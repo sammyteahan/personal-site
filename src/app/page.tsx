ab26b63959cc04e930bd5131bead6e5e8d3b2f0a
sammyteahan <sammyteahan@gmail.com>
2023-10-04 09:45:41 -0600

    Small style tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="h-[100vh] overflow-auto scrollbar-hide">
       <header className="flex max-w-[36rem] py-5 px-[13px] mx-auto w-full justify-between items-center">
-        <div className="bg-[#ffffffe6] drop-shadow-sm dark:bg-[#252529] px-4 py-2 border rounded-[14px] dark:border-[#2a2a2b] border-[#f4f4f5] ">
+        <div className="bg-[#ffffffe6] drop-shadow-sm dark:bg-[#252529] px-4 py-2 border rounded-[14px] dark:border-[#2a2a2b] border-[#f4f4f5]">
           <a className="mx-2 text-[14px] hover:text-[#7e7e7e] hover:dark:text-[#7e7e7e] dark:text-[#E7E7E4] text-[#2a2a2b]">Home</a>
         </div>
         <div className="flex w-full justify-end">
@@ -27,7 +27,6 @@ export default function Home() {
       </header>
 
       <main className="p-4 max-w-[36rem] mx-auto">
-        {/* info header section */}
         <div className="mt-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-center rounded-full">
