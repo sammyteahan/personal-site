7d1bbdaf9b3adf2e4201fede0a2608c31c076efc
James Teahan <sammyteahan@gmail.com>
2023-10-11 20:26:58 -0600

    Extract header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,16 @@
 "use client"
 
 import Image from 'next/image'
-import { useTheme } from "next-themes"
-import { Moon, Sun, Github, Linkedin, MoveUpRight } from "lucide-react"
+import { Github, Linkedin, MoveUpRight } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
+import Header from '@/components/header'
 import ExperienceRow from '@/components/experience-row'
 
 export default function Home() {
-  const { theme, setTheme } = useTheme()
-
-  const toggleTheme = () => theme === 'light'
-    ? setTheme('dark')
-    : setTheme('light')
 
   return (
     <div className="h-[100vh] overflow-auto scrollbar-hide">
-      <header className="flex max-w-[36rem] py-5 px-[13px] mx-auto w-full justify-between items-center">
-        <div className="bg-[#ffffffe6] drop-shadow-sm dark:bg-[#252529] px-4 py-2 border rounded-[14px] dark:border-[#2a2a2b] border-[#f4f4f5]">
-          <a className="mx-2 text-[14px] hover:text-[#7e7e7e] hover:dark:text-[#7e7e7e] dark:text-[#E7E7E4] text-[#2a2a2b] cursor-pointer">Home</a>
-        </div>
-        <div className="flex w-full justify-end">
-          <Button variant="outline" size="icon" onClick={toggleTheme}>
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
-          </Button>
-        </div>
-      </header>
+      <Header />
 
       <main className="p-4 max-w-[36rem] mx-auto">
         <div className="mt-4">
